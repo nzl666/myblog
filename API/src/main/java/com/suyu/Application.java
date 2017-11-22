9e80a66dbc2cbcedcf0deb58d799fa9838c3d580
聂志良 <niezhiliang@huluwa.cc>
2017-11-22 16:29:08 +0800

    m

--- API/src/main/java/com/suyu/Application.java
+++ API/src/main/java/com/suyu/Application.java
@@ -2,8 +2,10 @@ package com.suyu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = {"com.suyu"})
+@Import({com.suyu.Main.class})
 public class Application {
 
     public static void main(String[] args) {
